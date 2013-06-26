f602b403a2c3d85ef98cc8fd54f2b1b260eb034f
Bill McDaniel <bill@streamglider.com>
2013-06-26 15:11:38 -0500

    New comments section

--- StreamGlider App/Classes/BrowseTableViewController.m
+++ StreamGlider App/Classes/BrowseTableViewController.m
@@ -5,18 +5,17 @@
 //  Created by Dmitry Shingarev on 01/03/2011.
 //  Copyright 2011 StreamGlider, Inc. All rights reserved.
 //
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
+//  This program is free software if used non-commercially: you can redistribute it and/or modify
+//  it under the terms of the BSD 4 Clause License as published by
+//  the Free Software Foundation.
 //
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+//  BSD 4 Clause License for more details.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  You should have received a copy of the BSD 4 Clause License
+//  along with this program.  If not, see the README.md file with this program.
 
 #import "BrowseTableViewController.h"
 #import "FeedSourceCategory.h"
